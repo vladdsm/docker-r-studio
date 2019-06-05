76aab9facd8722177f8b249dfe493b994e06b35d
Viacheslav <z.slava@icloud.com>
2019-06-05 10:31:47 +0200

    Update install_packages.R

--- 02_code/install_packages.R
+++ 02_code/install_packages.R
@@ -14,6 +14,8 @@ install.packages("scales")
 install.packages("reshape2")
 install.packages("gapminder")
 install.packages("plumber")
+install.packages("DT")
+
 
 # The following two commands remove any previously installed H2O packages for R.
 if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
