67bc6454a3e61f97a40105ffd2605440f6c97dbf
Vlad <vladimir.zhbanko@gmail.com>
2020-10-27 12:44:25 +0100

    Update install_packages.R

--- 02_code/install_packages.R
+++ 02_code/install_packages.R
@@ -17,22 +17,10 @@ install.packages("plumber")
 install.packages("DT")
 install.packages("shinyjs")
 install.packages("V8")
+install.packages("h2o")
 install.packages("lazytrade")
 install.packages("translateR")
 install.packages("golem")
 install.packages("cranlogs")
 install.packages("shinydashboard")
-
-# The following two commands remove any previously installed H2O packages for R.
-if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
-if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
-
-# Next, we download packages that H2O depends on.
-pkgs <- c("RCurl","jsonlite")
-for (pkg in pkgs) {
-if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg) }
-}
-
-# Now we download, install and initialize the H2O package for R.
-install.packages("h2o", type="source", repos="http://h2o-release.s3.amazonaws.com/h2o/rel-zahradnik/1/R")
 
